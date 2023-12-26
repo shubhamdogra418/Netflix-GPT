a367958a6e828cd16603785757d78697683b6383
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-26 23:11:23 +0530

    SC- Movie LIst, Cards, APIs data fetched

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -2,10 +2,16 @@ import Header from './Header';
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import SecondaryContainer from './SecondaryContainer';
 import MainContainer from './MainContainer';
+import usePopularMovies from '../hooks/usePopularMovies';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
 
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useUpcomingMovies();
+  useTopRatedMovies();
 
 
   return (
